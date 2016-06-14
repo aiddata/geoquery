0b984c06819318692ea45d2016a02b546cf48c3a
eSlivinski <eslivinski@gmail.com>
2016-06-14 11:07:44 -0400

    geography search box

---
+++ www/public_src/scripts/controllers/geographySearchCtrl.js
@@ -0,0 +1,4 @@
+angular.module('aiddataDET')
+.controller('GeographySearchCtrl', function($scope, mapFactory) {
+
+});
--- www/public_src/scripts/factories/mapFactory.js
+++ www/public_src/scripts/factories/mapFactory.js
@@ -2,12 +2,14 @@ angular.module('aiddataDET')
 .factory('mapFactory', function() {
 
   var map = {};
+  var mapboxToken = 'pk.eyJ1IjoiZXNsaXZpbnNraWNhcnRvIiwiYSI6IjRWenpDcmMifQ.IU9qcKhUf_w-lTQQ-I7DIg';
 
   return {
     provision: function() {
-      map = L.map('map').setView([0, 0], 2);
+      map = L.map('map', {
+        zoomControl: false
+      }).setView([0, 0], 2);
 
-      var mapboxToken = 'pk.eyJ1IjoiZXNsaXZpbnNraWNhcnRvIiwiYSI6IjRWenpDcmMifQ.IU9qcKhUf_w-lTQQ-I7DIg';
       L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/emerald-v8/tiles/{z}/{x}/{y}?access_token=' + mapboxToken, {
         attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(map);
