5cf37c88ec7dfcb7fa173bfad9ff1478c87f1930
Alex Cavagnaro <alexander.cavagnaro@gmail.com>
2016-07-25 16:08:25 -0400

    welcome mode wip

--- www/public_src/scripts/controllers/map.mapCtrl.js
+++ www/public_src/scripts/controllers/map.mapCtrl.js
@@ -3,6 +3,37 @@ angular.module('aiddataDET')
 
   $scope.$on('$viewContentLoaded', function(event) {
     mapFactory.provision();
+    angular.module('DET', ['ngMaterial', 'ngMessages','material.svgAssetsCache'])
+    .controller('AppCtrl', function($scope, $mdDialog, $mdMedia) {
+      $scope.status = '';
+      $scope.customFullscreen = $mdMedia('x') || $mdMedia('sm');
+
+      $scope.showAlert = function(ev) {
+        $mdDialog.show()
+          $mdDialog.alert()
+
+      .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Welcome to the Data Extraction Tool by AidData!')
+          .textContent('This tool aims to influence the spread of aid by allowing users to analyze data based on boundaries, sectors, donors, and other filters.')
+          .ariaLabel('Welcome Modal')
+          .ok('Click Me!')
+          .targetEvent(ev)
+        );
+      };
+
+      function DialogController ($scope, $mdDialog) {
+        $scope.hide = function() {
+          $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+          $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+          $mdDialog.hide(answer);
+        };
+      }
+    })
 
     $timeout(function() { mapFactory.refreshSize(); });
   });
