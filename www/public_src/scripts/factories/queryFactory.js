0d2820a0762aa4aa9e946ec214d5c13d99121880
eSlivinski <eslivinski@gmail.com>
2016-08-02 12:55:35 -0400

    clean up

--- www/public_src/scripts/factories/queryFactory.js
+++ www/public_src/scripts/factories/queryFactory.js
@@ -1,5 +1,5 @@
 angular.module('aiddataDET')
-  .factory('queryFactory', function(ajaxFactory, $log, $q) {
+  .factory('queryFactory', function(ajaxFactory, $log, $q, $rootScope) {
 
     var _datasets = [],            // All Datasets
         _boundaries = {};          // All Boundaries
@@ -141,10 +141,10 @@ angular.module('aiddataDET')
 
         return $q.when(addFunct())
         .then(function(query) {
-          console.log(query);
           return query;
         });
       },
+
 
       isUniq: function(dataset, filters, options) {
         if (dataset.type === 'raster') {
@@ -293,13 +293,8 @@ angular.module('aiddataDET')
 
       updateFilterRange: function(min, max, filter) {
         var self = this;
-        this.filters[filter] = this.filters[filter] || [];
-
-        if (this.filters[filter].length) {
-          this.filters[filter].splice(0);
-        }
-        _.each(_.range(min, max + 1), function(n) {
-          self.filters[filter].push(n);
+        this.filters[filter] = _.filter(self.filterOptions.distinct[filter], function(f) {
+          return f >= min && f <= max;
         });
       },
 
@@ -310,6 +305,8 @@ angular.module('aiddataDET')
           this.filters[filter].splice(0);
         }
         this.filters[filter].push('All');
+
+        $rootScope.$broadcast('filters:resetRange', { filterId: filter });
       }
     };
   });
