81f83bf70c4f8eb526df87da82d14e4c9d22fde1
eSlivinski <eslivinski@gmail.com>
2016-08-06 22:45:57 -0400

    Pivotal 126987403

--- www/public_src/scripts/controllers/search.filtersCtrl.js
+++ www/public_src/scripts/controllers/search.filtersCtrl.js
@@ -1,5 +1,5 @@
 angular.module('aiddataDET')
-.controller('FiltersCtrl', function ($scope, $rootScope, $log, queryFactory, $stateParams, $state, filters, filterOptions) {
+.controller('FiltersCtrl', function ($scope, $rootScope, $log, $stateParams, $state, $timeout, $location, $anchorScroll, queryFactory, filters, filterOptions) {
   // $scope.filterOrder = [ Array holding the order filters should appear in ]
   // $scope.filterOptions = { Options for a particular filter field }
   // $scope.filters = { Active Filters }
@@ -19,8 +19,11 @@ angular.module('aiddataDET')
   };
 
   $scope.removeFilter = function(field) {
-    delete $scope.filters[field];
-    _.pull($scope.filterOrder, field);
+    $scope.dataset.fields[field].isActive = false;
+    $timeout(function () {
+      delete $scope.filters[field];
+      _.pull($scope.filterOrder, field);
+    }, 300);
   };
 
   $scope.$watch('filters', function (newValue, oldValue) {
@@ -29,12 +32,27 @@ angular.module('aiddataDET')
     }
   }, true);
 
+  $scope.$watch('filterOrder', function(newValue, oldValue) {
+    if (!_.difference(newValue, oldValue)[0]) { return; }
+    $timeout(function() {
+      $location.hash('addFilterButton');
+      $anchorScroll();
+      $scope.dataset.fields[_.difference(newValue, oldValue)[0]].isActive = true;
+    }, 300);
+
+  }, true);
+
   $scope.$on('$viewContentLoaded', function (event) {
     $scope.filterOptions = filterOptions;
     $scope.filterOrder = _.cloneDeep(filterOptions.filterTypes);
     $scope.filters = queryFactory.filters;
     $scope.dataset = queryFactory.getDataset();
     broadcastUpdates();
+
+    _.each($scope.filterOrder, function(f, i) {
+      $scope.dataset.fields[f].isActive = true;
+    });
+
   });
 
   function broadcastUpdates () {
