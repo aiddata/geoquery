e097068a3a44e394f57d69ec520ddc0f5aaf273c
eSlivinski <eslivinski@gmail.com>
2016-08-22 09:35:41 -0400

    Pivotal #128676027

--- www/public_src/scripts/controllers/search.filters.RangeCtrl.js
+++ www/public_src/scripts/controllers/search.filters.RangeCtrl.js
@@ -6,30 +6,11 @@ angular.module('aiddataDET')
 
   var min = _.min($scope.filterOptions),
       max = _.max($scope.filterOptions);
-  $scope.showSlider = false;
+
   $scope.range = { min: min, max: max };
-
-  $scope.sliderOptions = {};
-
-  $scope.fields = [
-    {
-      text: 'min',
-      validate: function (val) {
-        // $scope.range.min = val < $scope.sliderOptions.floor || val > $scope.range.max ? $scope.sliderOptions.floor : val;
-        $scope.updateRange();
-      }
-    },
-    {
-      text: 'max',
-      validate: function (val) {
-        // $scope.range.max = val < $scope.range.min || val > $scope.sliderOptions.ceil ? $scope.sliderOptions.ceil : val;
-        $scope.updateRange();
-      }
-    }
-  ];
+  $scope.sliderOptions = { visible: false };
 
   $scope.updateRange = function () {
-    console.log('update range');
     if (
       $scope.range.min === $scope.sliderOptions.floor &&
       $scope.range.max === $scope.sliderOptions.ceil &&
@@ -58,26 +39,21 @@ angular.module('aiddataDET')
     $scope.range.min = updateMin || $scope.range.min < min ? min : $scope.range.min;
     $scope.range.max = updateMax || $scope.range.max > max ? max : $scope.range.max;
 
-    $timeout(function () {
-      buildSlider();
-      $scope.updateRange();
-    }, 500);
-
+    $timeout(buildSlider, 500);
   }, true);
 
   function buildSlider () {
     $scope.sliderOptions = {
+      visible: true,
       floor: _.floor(min),
       ceil: _.ceil(max),
-      onEnd: function() { $scope.updateRange(); },
+      onEnd: $scope.updateRange,
       enforceRange: true,
       translate: function(val) { return '$' + val; }
     };
+
+    $scope.updateRange();
   }
 
-  buildSlider();
-  $timeout(function () {
-    $scope.showSlider = true;
-  }, 500);
 
 });
