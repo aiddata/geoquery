198b949c5114a126ce7cd338bfa690df1255d1ce
eSlivinski <eslivinski@gmail.com>
2016-09-09 14:51:48 -0400

    analytics

--- www/public_src/scripts/main.js
+++ www/public_src/scripts/main.js
@@ -251,4 +251,18 @@ angular.module('aiddataDET', ['ui.router', 'ui.bootstrap', 'ngMaterial', 'rzModu
 })
 .run(function($rootScope, config) {
   $rootScope.config = config;
+
+  /*
+   * ================
+   * Google Analytics
+   * ================
+   */
+
+  (function(i, s, o, g, r, a, m){ i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function(){
+    (i[r].q = i[r].q || []).push(arguments); }, i[r].l = 1 * new Date();a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+  ga('create', config.googleTrackingID, 'auto');
+  ga('send', 'pageview');
 });
