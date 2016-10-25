3782ba51a7b0f6af1c3f6070343f307699d6f60c
userz <sgoodman@aiddata.org>
2016-10-25 15:50:50 -0400

    Update bootstrapping scripts to make a bit easier to use.

--- bootstrapping/bootstrap.sh
+++ bootstrapping/bootstrap.sh
@@ -20,9 +20,16 @@ sudo apt-get update
   # upstart - runs webserver perpetually, after restarts
   # monit - for restarting webserver after crashes, and reporting
 
-sudo apt-get install -y unzip curl vim git nginx python-software-properties python g++ make nodejs upstart monit
 
-sudo cp /vagrant/bootstrapping/nodeserver_nginx.conf /etc/nginx/sites-available/
+boot_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+root_dir="$( dirname ${boot_dir} )"
+web_dir=/var/www
+
+sudo apt-get install -y unzip curl vim git nginx python-software-properties python g++ make nodejs npm upstart monit supervisor
+
+sudo ln -s "$(which nodejs)" /usr/bin/node
+
+sudo cp /${root_dir}/bootstrapping/nodeserver_nginx.conf /etc/nginx/sites-available/
 sudo rm /etc/nginx/sites-enabled/default
 
 sudo ln -s /etc/nginx/sites-available/nodeserver_nginx.conf /etc/nginx/sites-enabled/nodeserver_nginx.conf
@@ -32,17 +39,25 @@ sudo service nginx reload
 
 sudo npm config set registry http://registry.npmjs.org/
 
-cd /vagrant/www
+sudo rm -r ${web_dir}/*
+sudo cp -rT /${root_dir}/www ${web_dir}
+cd ${web_dir}
 
 sudo npm install
 sudo npm install --save -g supervisor
 
-cd /vagrant/bootstrapping
+sudo npm install -g grunt-cli
+sudo npm install -g bower
+
+cd /${root_dir}/bootstrapping
 
 
 # copy conf files over
-sudo cp nodeserver.conf /etc/init/
+sudo cp nodeserver.conf nodeserver.conf.active
+sudo sed -i "s#WEB_DIR#$web_dir#g" nodeserver.conf.active
+sudo cp nodeserver.conf.active /etc/init/nodeserver.conf
 sudo chmod 777 /etc/init/nodeserver.conf
+sudo rm nodeserver.conf.active
 
 sudo cp nodeserver_monit.conf /etc/monit/conf.d/
 sudo chmod 700 /etc/monit/conf.d/nodeserver_monit.conf
