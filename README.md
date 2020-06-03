256a7bd00509dc6c320bebb349535b7bc9aa5527
sgoodm <sgoodman@aiddata.wm.edu>
2020-06-03 16:06:27 -0400

    Update for Ubuntu 18.04

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-## AidData-DET
-The Data Extract Tool (DET) was developed for AidData by NBT Solutions in 2016.
-For information on configuring and deploying the application, please see the [DET wiki](https://github.com/NBTSolutions/aidData-DET/wiki).
+## AidData GeoQuery
+
+AidData's [GeoQuery](https://geoquery.org) tool for accessing and extractinggeospatial data was developed for AidData by NBT Solutions in 2016.
+For information on configuring and deploying the application, please see the [wiki](https://github.com/aiddata/geo-query/wiki).
--- bootstrapping/bootstrap.sh
+++ bootstrapping/bootstrap.sh
@@ -1,9 +1,11 @@
+# updated for ubuntu 18.04
 
 ubuntu_version=$(lsb_release -sr)
 echo Running Ubuntu ${ubuntu_version}
 
-
 boot_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# boot_dir=/opt/aiddata/geo-query/bootstrapping
+
 root_dir="$( dirname ${boot_dir} )"
 
 
@@ -23,7 +25,6 @@ else
 fi
 
 
-sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 
 # RUN OUR INSTALLS:
@@ -42,13 +43,15 @@ sudo apt-get update
 
 # WEBSERVER UTILS
   # nodejs
-  # upstart - runs webserver perpetually, after restarts
   # monit - for restarting webserver after crashes, and reporting
 
 
-sudo apt-get install -y unzip curl vim git nginx python-software-properties python g++ make nodejs upstart monit npm nano
+sudo apt install -y unzip curl vim git nginx software-properties-common python g++ make nano monit
 
-sudo ln -s "$(which nodejs)" /usr/bin/node
+# https://github.com/nodesource/distributions#debinstall
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 
 sudo cp /${boot_dir}/nodeserver_nginx.conf /etc/nginx/sites-available/
 sudo rm /etc/nginx/sites-enabled/default
@@ -56,17 +59,8 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/nodeserver_nginx.conf /etc/nginx/sites-enabled/nodeserver_nginx.conf
 
 
-if [[ $ubuntu_version == "14.04" ]]; then
-
-    sudo service nginx start
-    sudo service nginx reload
-
-elif [[ $ubuntu_version == "16.04" ]]; then
-
-    sudo systemctl enable nginx
-    sudo systemctl start nginx
-
-fi
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
 
 sudo npm config set registry http://registry.npmjs.org/
@@ -88,39 +82,26 @@ sudo npm install --save -g supervisor
 sudo npm install -g grunt-cli
 sudo npm install -g bower
 
+sudo ln -s /usr/bin/supervisor /usr/local/bin/supervisor
+
 
 # copy conf files over
 cd ${boot_dir}
 
 
-if [[ $ubuntu_version == "14.04" ]]; then
 
-    # upstart
-    sudo cp nodeserver.conf nodeserver.conf.active
-    sudo sed -i "s#WEB_DIR#$web_dir#g" nodeserver.conf.active
-    sudo cp nodeserver.conf.active /etc/init/nodeserver.conf
-    sudo chmod 777 /etc/init/nodeserver.conf
-    sudo rm nodeserver.conf.active
+# systemd
+sudo touch /var/log/nodeserver.sys.log
+sudo chmod 666 /var/log/nodeserver.sys.log
 
-    # start the node server daemon
-    sudo start nodeserver
+sudo cp nodeserver.service nodeserver.service.active
+sudo sed -i "s#WEB_DIR#$web_dir#g" nodeserver.service.active
+sudo cp nodeserver.service.active /lib/systemd/system/nodeserver.service
+sudo chmod 777 /lib/systemd/system/nodeserver.service
 
-elif [[ $ubuntu_version == "16.04" ]]; then
-
-    # systemd
-    sudo touch /var/log/nodeserver.sys.log
-    sudo chmod 666 /var/log/nodeserver.sys.log
-
-    sudo cp nodeserver.service nodeserver.service.active
-    sudo sed -i "s#WEB_DIR#$web_dir#g" nodeserver.service.active
-    sudo cp nodeserver.service.active /lib/systemd/system/nodeserver.service
-    sudo chmod 777 /lib/systemd/system/nodeserver.service
-
-    # start the node server daemon
-    sudo systemctl enable nodeserver
-    sudo systemctl start nodeserver
-
-fi
+# start the node server daemon
+sudo systemctl enable nodeserver
+sudo systemctl start nodeserver
 
 
 sudo cp nodeserver_monit.conf /etc/monit/conf.d/
@@ -129,8 +110,6 @@ sudo chmod 700 /etc/monit/conf.d/nodeserver_monit.conf
 
 # start monit for error checking
 sudo monit -d 10 -c /etc/monit/conf.d/nodeserver_monit.conf
-
-
 
 
 cd ${web_dir}/public_src/libs
