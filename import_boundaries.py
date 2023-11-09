53584369d50c8172e6d782b6c256ef2a97b007a6
Jacob Hall <email@jacobhall.net>
2023-11-09 14:59:06 -0500

    add sampling options, cli support to import_boundaries.py

--- import_boundaries.py
+++ import_boundaries.py
@@ -1,7 +1,10 @@
 from time import sleep
 from pathlib import Path
+from typing import Optional
 
+import click
 import psycopg
+from psycopg.errors import UniqueViolation
 import shapely
 from tqdm import tqdm
 import geopandas as gpd
@@ -9,7 +12,7 @@ import geopandas as gpd
 from conn import get_conn
 
 # function to insert row from geopandas df into boundaries table
-def insert_feature(cur, dataset_id, adm_level, row):
+def insert_feature(cur, dataset_id, adm_level, row) -> None:
 
     wkt = shapely.to_wkt(row["geometry"])
     
@@ -38,30 +41,57 @@ def insert_feature(cur, dataset_id, adm_level, row):
         """,
         (dataset_id, result, row["shapeName"], row["shapeGroup"], adm_level))
 
+def run_import(sample: Optional[int]=False) -> None:
 
-if __name__ == "__main__":
-    dataset_name = "gbv6"
+    if sample and sample < 1:
+        raise ValueError("sample must be a positive integer")
+
+    dataset_name: str = "gbv6"
+
+    # GeoPandas GeoSeries of group names to exclusively import
+    # populated after init of first GeoDataFrame below
+    # only used if sample is not None
+    sampled_groups: gpd.GeoSeries = gpd.GeoSeries()
 
     with get_conn(autocommit=True) as conn:
         with conn.cursor() as cur:
 
-            cur.execute("INSERT INTO datasets (name) VALUES (%s) RETURNING id", [dataset_name])
-            dataset_id = cur.fetchone()[0]
-
-            vac_count = 0
+            try:
+                # insert dataset name, get its associated id
+                cur.execute("INSERT INTO datasets (name) VALUES (%s) RETURNING id", [dataset_name])
+                dataset_id = cur.fetchone()[0]
+            except UniqueViolation:
+                raise UniqueViolation(f"dataset {dataset_name} already exists in database!")
 
             # for each ADM level file downloaded from geoBoundaries
             for adm_level in range(3):
-                print(f"Loading ADM{adm_level}...")
                 src = Path(f"geoBoundariesCGAZ_ADM{adm_level}.gpkg") 
+
                 # load this file into a geopandas df
-                df = gpd.read_file(src)
+                print(f"Loading ADM{adm_level}...", end="")
+                df: gpd.GeoDataFrame = gpd.read_file(src)
+                print("done")
+
+                if sample:
+                    # if this is the first iteration, select groups to sample
+                    if adm_level == 0:
+                        selected_groups = df["shapeGroup"].unique()[:sample]
+
+                    # filter df to only include selected groups
+                    df = df[df["shapeGroup"].isin(selected_groups)]
+
                 # iterate through rows of df
                 # NOTE: there may be faster ways to iterate
                 for row in tqdm(df.to_dict(orient="records")):
-                    if vac_count > 20:
-                        # conn.commit()
-                        # cur.execute("VACUUM ANALYZE features;")
-                        vac_count = 0
                     insert_feature(cur, dataset_id, adm_level, row)
-                    vac_count += 1
+
+
+@click.command()
+@click.option('--sample', default=False, type=int)
+def main(sample: bool) -> None:
+    if sample:
+        print(f"WARNING: Only importing a sample of {sample} countrys' boundaries for testing.")
+    run_import(sample=sample)
+
+if __name__ == "__main__":
+    main()
