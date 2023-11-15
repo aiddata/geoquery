7a38dae89e7ef37f9ccaf257a150d47e1cdfcf0f
Jacob Hall <email@jacobhall.net>
2023-11-15 13:27:29 -0500

    initial commit of insert_request.py

---
+++ insert_request.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from pydantic import BaseModel, field_validator
+
+from conn import get_conn
+
+
+class Request(BaseModel):
+    date: datetime
+    source: str
+    contact: str
+    custom_name: str
+    info: str
+    priority: int = 1
+
+    @field_validator("priority")
+    @classmethod
+    def function_must_exist(cls, p: int) -> int:
+        if p < 1:
+            raise ValueError("request priority must be at least 1")
+        return p
+
+def insert_request(request: Request):
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            query = "" # TODO
+
+            cur.execute(query, request)
+
+            # TODO: insert into extract_tasks, request_map
