4e102efeec9afe13ff42760ad629bcc8613ba76b
Jacob Hall <email@jacobhall.net>
2023-11-16 14:20:41 -0500

    fix typos in zonal_stats_rasterstats.py

--- src/utils/processors/zonal_stats_rasterstats.py
+++ src/utils/processors/zonal_stats_rasterstats.py
@@ -1,7 +1,6 @@
 # module for processor functions
 import rasterstats as rs
-
-from utils.helpers import get_feat_by_id, get_dataset_resource_path_by_id
+from utils.helpers import get_dataset_resource_path_by_id, get_feat_by_id
 
 
 def _rasterstats_default(feature_id, dataset_resource_id, stat):
@@ -15,31 +14,35 @@ def _rasterstats_default(feature_id, dataset_resource_id, stat):
 def exists(self, name):
     return hasattr(self, name) and callable(getattr(self, name))
 
+
 def rasterstats_default_min(feature_id, dataset_resource_id):
     output = _rasterstats_default(feature_id, dataset_resource_id, "min")
     return output
+
 
 def rasterstats_default_max(feature_id, dataset_resource_id):
     output = _rasterstats_default(feature_id, dataset_resource_id, "max")
     return output
 
-def rasterstats_default_max(feature_id, dataset_resource_id):
+
+def rasterstats_default_mean(feature_id, dataset_resource_id):
     output = _rasterstats_default(feature_id, dataset_resource_id, "mean")
     return output
 
-def rasterstats_default_max(feature_id, dataset_resource_id):
+
+def rasterstats_default_sum(feature_id, dataset_resource_id):
     output = _rasterstats_default(feature_id, dataset_resource_id, "sum")
     return output
 
-def rasterstats_default_max(feature_id, dataset_resource_id):
+
+def rasterstats_default_count(feature_id, dataset_resource_id):
     output = _rasterstats_default(feature_id, dataset_resource_id, "count")
     return output
+
 
 def rasterstats_default_categorical(feature_id, dataset_resource_id, mapping):
     feat = get_feat_by_id(feature_id)
     raster = get_dataset_resource_path_by_id(dataset_resource_id)
-    stats = rs.zonal_stats(
-        feat, raster, categorical=True, category_map=mapping
-    )
+    stats = rs.zonal_stats(feat, raster, categorical=True, category_map=mapping)
     output = [(f"categorical_{k}", v) for k, v in stats.items()]
     return output
