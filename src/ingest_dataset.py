4bd9d1cd6db9086a9c2885f0fd3ad3f4af9e3a81
sgoodm <sgoodman@aiddata.wm.edu>
2023-11-16 11:58:55 -0500

    Fix pathing / module usage

--- src/ingest_dataset.py
+++ src/ingest_dataset.py
@@ -2,16 +2,16 @@
 import json
 from pathlib import Path
 
-from utils.db import insert_dataset
+from utils.db import insert_dataset as id
 
 
 
-json_path = Path("data/esa_lc/raster_ingest.json")
+json_path = Path("../data/esa_lc/raster_ingest.json")
 
 json_data = json.loads(json_path.read_text())
 
 
-DS = insert_dataset.Dataset(**json_data)
+DS = id.Dataset(**json_data)
 
 id.insert_dataset(DS)
 id.update_dataset(DS)
