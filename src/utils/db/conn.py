98bfa7aca06a7b6811590ccfe581ed31ecb936da
sgoodm <sgoodman@aiddata.wm.edu>
2023-11-18 17:32:41 -0500

    Update db connection to use dict instead of indices for result cols

--- src/utils/db/conn.py
+++ src/utils/db/conn.py
@@ -1,11 +1,14 @@
 from contextlib import contextmanager
 
 import psycopg
+from psycopg.rows import dict_row
 
 
 @contextmanager
 def get_conn(**kwargs):
     # TODO: configurable connection options, e.g. host and password
+    if "row_factory" not in kwargs:
+        kwargs["row_factory"] = dict_row
     with psycopg.connect(
         "postgresql://postgres:mysecretpassword@localhost:5432", **kwargs
     ) as conn:
