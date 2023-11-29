14efe331e64f35d4bbae1a32d899dfc18caeecab
Jacob Hall <email@jacobhall.net>
2023-11-29 14:51:20 -0500

    updates to extract task LockTask context manager

--- src/process_extract_tasks.py
+++ src/process_extract_tasks.py
@@ -1,16 +1,25 @@
 import builtins
 from datetime import datetime
+from pathlib import Path
 from typing import Any, Dict, List, Optional
 
-import psycopg
-import rasterstats as rs
-import shapely
-from psycopg.types.json import Jsonb
-from pydantic import BaseModel, Json, ValidationInfo, field_validator
-
 import utils.processors
-from utils.db.conn import get_conn
-from utils.db.extract_task_processing import ExtractData, LockTask
+from utils.db.extract_task_processing import (ExtractData, LockTask,
+                                              get_mappings)
+
+
+def run_op(feat, data, func, **kwargs):
+    results = func(feat, data, **kwargs)
+    return results
+
+
+def get_func(op):
+    """Get appropriate function for operation."""
+    if hasattr(utils.processors, op):
+        func = getattr(utils.processors, op)
+    else:
+        raise ValueError(f"Operation {op} not supported.")
+    return func
 
 
 def run_extract():
@@ -27,29 +36,25 @@ def run_extract():
     # get a task, locked just for us!
     with LockTask() as task:
         if task.data is not None:
-            feat = get_feat(task.data.fm_id)
-            resource = get_dataset_resource(task.data.resource_id)
-            dataset = get_dataset(resource["dataset_id"])
-            path = dataset["path"] + "/" + resource["path"]
-            po = get_processing_option(task.data.po_id)
+            path = Path(task.data.dataset_path) / task.data.resource_path
+            func = get_func(task.data.po_func)
+            op_kwargs = {"stat": task.data.po_short_name}
 
-            func = get_func(po["function"])
-
-            method = po["short_name"]
-
-            op_kwargs = {"stat": method}
-            if dataset["mapped"] == True:
-                map = get_mappings(dataset["id"])
+            # TODO: I'd prefer if get_mappings was done internally by LockTask,
+            #       and exposed within ExtractTaskToRun
+            if task.data.mapped_dataset:
+                map = get_mappings(task.data.dataset_id)
                 op_kwargs["category_map"] = {i["map_val"]: i["map_name"] for i in map}
 
-            result = run_op(feat, path, func, **op_kwargs)
+            result = run_op(task.data.feature, path, func, **op_kwargs)
 
             for method, val in result:
-                # FIXME: this likely doesn't need to exist. At the end of the day,
-                #        it should be Postgres that checks result type on insert
-                #        and raise an error if there was a bad insertion.
-                #        At the very least, our context manager should check for us.
-                val = getattr(builtins, po["result_type"])(val)
+                # FIXME: the following (commented) line doesn't need to exist.
+                #        At the end of the day, it should be Postgres that
+                #        checks result type on insert and raise an error if
+                #        there was a bad insertion. At the very least, our
+                #        context manager should check for us.
+                # val = getattr(builtins, po["result_type"])(val)
 
                 result = ExtractData(
                     id=task.data.id,
@@ -63,113 +68,5 @@ def run_extract():
                 Warning("No available tasks to complete!")
 
 
-def update_extract_task(task_id, status, update_type):
-    """Update the status of an extract task"""
-    if update_type not in ["update", "complete"]:
-        raise ValueError(f"Update type {update_type} not supported.")
-    update_str = f"{update_type}_time"
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                psycopg.sql.SQL(
-                    """
-                UPDATE extract_tasks
-                SET    status = %s, {} = CURRENT_TIMESTAMP
-                WHERE  id = %s;
-                """
-                ).format(psycopg.sql.Identifier(update_str)),
-                (status, task_id),
-            )
-
-
-def get_feat(fid):
-    """GET FEATURE FROM FEATURE TABLE"""
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            feat = cur.execute(
-                """SELECT * FROM features WHERE id = %s""", (fid,)
-            ).fetchone()
-    geom = shapely.wkb.loads(feat["shape"], hex=True)
-
-    return geom
-
-
-def get_dataset_resource(resource_id):
-    """GET DATASET RESOURCE FROM DATASET RESOURCE TABLE"""
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            resource = cur.execute(
-                """SELECT * FROM dataset_resources WHERE id = %s""", (resource_id,)
-            ).fetchone()
-
-    return resource
-
-
-def get_dataset(did):
-    """GET DATA META FROM DATA TABLE AND ASSOCIATED PATH/INFO FOR ACTUAL DATASET"""
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            data = cur.execute(
-                """SELECT * FROM datasets WHERE id = %s""", (did,)
-            ).fetchone()
-
-    return data
-
-
-def get_processing_option(po_id):
-    """Get processing option from processing options table."""
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            po = cur.execute(
-                """SELECT * FROM processing_options WHERE id = %s""", (po_id,)
-            ).fetchone()
-
-    return po
-
-
-def get_mappings(dataset_id):
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            mappings = cur.execute(
-                """SELECT * FROM mappings WHERE dataset_id = %s""", (dataset_id,)
-            ).fetchall()
-    return mappings
-
-
-def run_op(feat, data, func, **kwargs):
-    results = func(feat, data, **kwargs)
-    return results
-
-
-def export_result(result):
-    """EXPORT RESULT TO EXTRACT RESULT TABLE"""
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            status = insert_result(cur, result)
-    return status
-
-
-def insert_result(cur, result):
-    cur.execute(
-        """
-        INSERT INTO extract_data (id, name, data_column, float_value, int_value, str_value)
-        VALUES (%s, %s, %s, %s, %s, %s);
-        """,
-        (
-            result.id,
-            result.name,
-            result.data_column,
-            result.float_value,
-            result.int_value,
-            result.str_value,
-        ),
-    )
-
-
-def get_func(op):
-    """Get appropriate function for operation."""
-    if hasattr(utils.processors, op):
-        func = getattr(utils.processors, op)
-    else:
-        raise ValueError(f"Operation {op} not supportedx.")
-    return func
+if __name__ == "__main__":
+    run_extract()
--- src/utils/db/extract_task_processing.py
+++ src/utils/db/extract_task_processing.py
@@ -1,24 +1,58 @@
 from datetime import datetime
+from pathlib import Path
 from typing import Any, Dict, List, Optional, Union
 
+import shapely
 from psycopg import Connection, Cursor
 from psycopg.rows import class_row
-from pydantic import BaseModel, Json, ValidationInfo, field_validator
+from pydantic import BaseModel, ConfigDict, field_validator
+from shapely import Geometry
 from typing_extensions import Self
 
-from utils.db.conn import get_conn
-from extract_task_generation import ExtractTask
+from .conn import get_conn
 
 
-class ExtractTaskFromDB(ExtractTask):
+class ExtractTaskToRun(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     id: int
+    dataset_id: int
+    dataset_path: Union[str, Path]
+    mapped_dataset: bool
+    resource_path: Path
+    po_func: str
+    po_short_name: str
+    feature: Union[str, Geometry]
+
+    @field_validator("dataset_path")
+    @classmethod
+    def convert_dataset_path(cls, p: Union[str, Path]) -> Path:
+        if isinstance(p, str):
+            return Path(p)
+        else:
+            return p
+
+    @field_validator("resource_path")
+    @classmethod
+    def convert_resource_path(cls, p: Union[str, Path]) -> Path:
+        if isinstance(p, str):
+            return Path(p)
+        else:
+            return p
+
+    @field_validator("feature")
+    @classmethod
+    def convert_feature(cls, g: Union[str, Geometry]) -> Geometry:
+        if isinstance(g, str):
+            return shapely.wkb.loads(g, hex=True)
+        else:
+            return g
 
 
 class ExtractData(BaseModel):
     id: int
     name: str
     value: Union[int, float, str]
-
 
 
 def _insert_extract_data(cur: Cursor, data: ExtractData) -> None:
@@ -57,65 +91,119 @@ def _insert_extract_data(cur: Cursor, data: ExtractData) -> None:
     cur.execute(add_result_query, params)
 
 
+def get_mappings(dataset_id):
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            mappings = cur.execute(
+                """SELECT * FROM mappings WHERE dataset_id = %s""", (dataset_id,)
+            ).fetchall()
+    return mappings
+
 
 class LockTask:
     conn: Connection
-    cur: Cursor
-    data: Optional[ExtractTaskFromDB]
+    data: Optional[ExtractTaskToRun]
+    results: List[ExtractData] = []
 
     def __enter__(self) -> Self:
         # open connection to database
         self.conn_context = get_conn()
         self.conn = self.conn_context.__enter__()
-        self.cur = self.conn.cursor(row_factory=class_row(ExtractTaskFromDB))
 
-        # select an unlocked task from extract_tasks, locking it
-        # TODO: only select tasks that have fewer than X number of previous errors?
-        #       ...this would require better error tracking, see comment in self.__exit__() below
-        select_task_query = """
-            SELECT *
-            FROM extract_tasks
-            WHERE status = 0
-            ORDER BY priority DESC, submit_time ASC
-            LIMIT  1
-            FOR UPDATE SKIP LOCKED
-        """
-        task = self.cur.execute(select_task_query)
-        task_result = task.fetchone()
+        with self.conn.cursor(row_factory=class_row(ExtractTaskToRun)) as cur:
+            # select an unlocked task from extract_tasks, locking it
+            # TODO: only select tasks that have fewer than X number of previous errors?
+            #       ...this would require better error tracking, see comment in self.__exit__() below
 
-        if task_result is None:
-            # there were no unlocked tasks!
-            self.data = None
-        else:
-            self.data = task_result
+            select_task_query = """
+                UPDATE extract_tasks
+                SET status = 2
+                FROM (
+                    SELECT
+                        task_id,
+                        datasets.id AS dataset_id,
+                        datasets.path AS dataset_path,
+                        datasets.mapped AS mapped_dataset,
+                        dataset_resources.path AS resource_path,
+                        processing_options.function AS po_func,
+                        processing_options.short_name AS po_short_name,
+                        features.shape AS feature
+                    FROM (
+                        SELECT
+                            id AS task_id,
+                            fm_id,
+                            resource_id,
+                            po_id AS processing_option_id
+                        FROM extract_tasks
+                        WHERE status = 0
+                        ORDER BY priority DESC, submit_time ASC
+                        LIMIT  1
+                        FOR UPDATE SKIP LOCKED
+                    )
+                    LEFT OUTER JOIN feat_map
+                        ON fm_id = feat_map.id
+                    LEFT OUTER JOIN features
+                        ON geom_id = features.id
+                    LEFT OUTER JOIN dataset_resources
+                        ON resource_id = dataset_resources.id
+                    LEFT OUTER JOIN datasets
+                        ON dataset_resources.dataset_id = datasets.id
+                    LEFT OUTER JOIN processing_options
+                        ON processing_option_id = processing_options.id
+                )
+                WHERE extract_tasks.id = task_id
+                RETURNING *;
+            """
+            task = cur.execute(select_task_query)
+            task_result = task.fetchone()
 
-        # TODO: ensure that a dropped connection (plug gets pulled) means that task gets unlocked
-        #       ...this might require some kind of idle timeout procedure? Need to investigate
+            if task_result is None:
+                # there were no unlocked tasks!
+                self.data = None
+            else:
+                self.data = task_result
 
-        return self
+            return self
 
     def submit_result(self, data: ExtractData) -> None:
         # TODO: either assert that data.id == self.data.id,
         #       or build ExtractData here from parameters to enforce this
-        _insert_extract_data(self.cur, data)
+        self.results.append(data)
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
         # TODO: log error somewhere, if there was one? check if a result was submitted?
         #       we might want to create an extract tasks error table
 
-        # if no error was thrown, and we were working on a real task, task is complete!
-        if exc_type is None and self.data is not None:
-            mark_as_complete_query = """
-                UPDATE extract_tasks
-                SET status = 1
-                WHERE id = %s
-            """
-            self.cur.execute(mark_as_complete_query, (self.data.id,))
+        if self.data is not None:
+            with self.conn.cursor() as cur:
+                # if no error was thrown, and we got results, let's insert!
+                if exc_type is None and len(self.results) > 0:
+                    for result in self.results:
+                        _insert_extract_data(cur, result)
 
-        # commit our changes, including all extract data insertions and marking as complete
+                    mark_as_complete_query = """
+                        UPDATE extract_tasks
+                        SET status = 1
+                        WHERE id = %s
+                    """
+                    cur.execute(mark_as_complete_query, (self.data.id,))
+                else:
+                    mark_as_error_query = """
+                        UPDATE extract_tasks
+                        SET status = -1
+                        WHERE id = %s
+                    """
+                    cur.execute(mark_as_error_query, (self.data.id,))
+
+        # commit our changes, including all extract data
+        # insertions and marking as complete
         self.conn.commit()
 
         # closing connection closes transaction, releasing lock
         self.conn.close()
 
         self.conn_context.__exit__(None, None, None)
+
+
+if __name__ == "__main__":
+    generate_tasks(overwrite=True)
