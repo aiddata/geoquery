133c383f9ee5d16d9e86ead087ba9eb3bff5e867
sgoodm <sgoodman@aiddata.wm.edu>
2023-11-30 15:26:47 -0500

    Use nodata value from processing option kwargs in extract funcs

--- src/gqcore/utils/processors/zonal_stats_rasterstats.py
+++ src/gqcore/utils/processors/zonal_stats_rasterstats.py
@@ -2,40 +2,42 @@
 import rasterstats as rs
 
 
-def _rasterstats_default(feat, raster, stat):
-    stats = rs.zonal_stats(feat, raster, stats=stat)
+def _rasterstats_default(feat, raster, stat, **kwargs):
+    nodata = kwargs["nodata"] if "nodata" in kwargs else None
+    stats = rs.zonal_stats(feat, raster, stats=stat, nodata=nodata)
     output = stats[0][stat]
     return [(stat, output)]
 
 
-def rasterstats_default_min(feat, raster):
-    output = _rasterstats_default(feat, raster, "min")
+def rasterstats_default_min(feat, raster, **kwargs):
+    output = _rasterstats_default(feat, raster, "min", **kwargs)
     return output
 
 
-def rasterstats_default_max(feat, raster):
-    output = _rasterstats_default(feat, raster, "max")
+def rasterstats_default_max(feat, raster, **kwargs):
+    output = _rasterstats_default(feat, raster, "max", **kwargs)
     return output
 
 
-def rasterstats_default_mean(feat, raster):
-    output = _rasterstats_default(feat, raster, "mean")
+def rasterstats_default_mean(feat, raster, **kwargs):
+    output = _rasterstats_default(feat, raster, "mean", **kwargs)
     return output
 
 
-def rasterstats_default_sum(feat, raster):
-    output = _rasterstats_default(feat, raster, "sum")
+def rasterstats_default_sum(feat, raster, **kwargs):
+    output = _rasterstats_default(feat, raster, "sum", **kwargs)
     return output
 
 
-def rasterstats_default_count(feat, raster):
-    output = _rasterstats_default(feat, raster, "count")
+def rasterstats_default_count(feat, raster, **kwargs):
+    output = _rasterstats_default(feat, raster, "count", **kwargs)
     return output
 
 
 def rasterstats_default_categorical(feat, raster, **kwargs):
     mapping = kwargs["category_map"]
-    stats = rs.zonal_stats(feat, raster, categorical=True, category_map=mapping)
+    nodata = kwargs["nodata"] if "nodata" in kwargs else None
+    stats = rs.zonal_stats(feat, raster, categorical=True, category_map=mapping, nodata=nodata)
     output = [(f"categorical_{k}", v) for k, v in stats[0].items()]
 
     for v, k in mapping.items():
