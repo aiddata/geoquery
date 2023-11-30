09af9462020ffa98be1280adddb89e3a95cd47a4
sgoodm <sgoodman@aiddata.wm.edu>
2023-11-30 10:26:51 -0500

    Add the file for generalize feat collection ingest

---
+++ src/ingest_feature_collection.py
@@ -0,0 +1,23 @@
+
+import json
+from pathlib import Path
+
+from utils.db import features as futils
+
+
+def ingest_feature_collection(json_path: str = None, json_data: dict = None, update: bool = False) -> None:
+    if json_path is None and json_data is None:
+        raise ValueError("Must provide either json_path or json_data")
+    elif json_path is not None and json_data is not None:
+        raise ValueError("Must provide either json_path or json_data, not both")
+    elif json_path is not None:
+        path = Path(json_path)
+        data = json.loads(path.read_text())
+    else:
+        data = json_data
+
+    FC = futils.FeatureCollection(**data)
+    if update:
+        futils.update_feature_collection(FC)
+    else:
+        futils.insert_feature_collection(FC)
