8e4e154b7af0dade3db4547ee4d17a4b4994350a
sgoodm <sgoodman@aiddata.wm.edu>
2023-12-01 15:51:17 -0500

    Update ingest

--- ingest/esa_lc.py
+++ ingest/esa_lc.py
@@ -1,3 +1,3 @@
 from gqcore.ingest_dataset import ingest_dataset
 
-ingest_dataset(json_path="/home/userx/Desktop/geoquery-update/data/esa_lc/raster_ingest.json", update=True)
+ingest_dataset(json_path="/home/userx/Desktop/geoquery-update/data/esa_lc/raster_ingest.json", update=False)
