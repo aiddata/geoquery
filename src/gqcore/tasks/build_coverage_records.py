35bb6f08e0969d1aa36976aca785954bcec2bd69
sgoodm <sgoodman@aiddata.wm.edu>
2023-12-12 11:10:24 -0500

    Test logging

--- src/gqcore/tasks/build_coverage_records.py
+++ src/gqcore/tasks/build_coverage_records.py
@@ -1,21 +1,39 @@
-
+import sys
 import itertools
+
+from loguru import logger
 
 from gqcore.utils.models import CoverageRecord
 from gqcore.utils.db.helpers import get_coverage_records, get_dataset_ids_without_coverage_dependencies, get_feature_ids, get_feat_geom_by_id, get_dataset_extent_by_id, update_coverage_status, insert_coverage_records
 
+logger.add("/home/userx/Desktop/geoquery-update/logs/main.log",
+           format="{time} {level} {message}",
+           rotation="1 week",
+           level="DEBUG",
+           backtrace=True,
+           diagnose=True,
+)
+
 
 def generate_coverage_records():
+
+    logger.info("Generating coverage records")
 
     existing_coverage = get_coverage_records()
     existing_items = [(x["geom_id"], x["dataset_id"]) for x in existing_coverage]
 
+    # TODO: replace query to get feature/dataset ids with single query that only
+    # returns ids for which the pair does not exist in coverage table
+    
     feature_ids = get_feature_ids()
     if len(feature_ids) == 0:
-        raise ValueError("No features found in database")
+        logger.info("No features found in database")
+        return
+
     dataset_ids = get_dataset_ids_without_coverage_dependencies()
     if len(dataset_ids) == 0:
-        raise ValueError("No datasets found in database")
+        logger.info("No datasets found in database")
+        return
 
     potential_coverage = list(itertools.product(feature_ids, dataset_ids))
 
@@ -26,14 +44,20 @@ def generate_coverage_records():
 
     insert_coverage_records(new_coverage)
 
+    logger.success("Coverage records generated")
+
 
 def test_coverage():
+
+    logger.info("Testing coverage for untested records")
 
     coverage_items = get_coverage_records(status=-1)
 
     for c in coverage_items:
         feature_id = c["geom_id"]
         dataset_id = c["dataset_id"]
+
+        logger.info(f"Testing coverage for feature {feature_id} and dataset {dataset_id}", extra={"feature_id": feature_id, "dataset_id": dataset_id})
 
         feature_geom = get_feat_geom_by_id(feature_id)
 
@@ -49,6 +73,8 @@ def test_coverage():
             new_status = 0
 
         update_coverage_status(feature_id, dataset_id, new_status)
+
+    logger.success("Coverage testing complete")
 
 
 if __name__ == "__main__":
