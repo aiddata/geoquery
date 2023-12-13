0061d7665054be0525593d131221165d4cb1f2f3
sgoodm <sgoodman@aiddata.wm.edu>
2023-12-13 12:07:00 -0500

    Add logging to ingest utils for FC and datasets

--- src/gqcore/utils/ingest/ingest_dataset.py
+++ src/gqcore/utils/ingest/ingest_dataset.py
@@ -6,30 +6,40 @@ from loguru import logger
 from gqcore.utils.db import dataset as dutils
 
 
-@logger.catch
+@logger.catch(reraise=True)
 def ingest_dataset(
     json_path: str = None,
     json_data: dict = None,
     update: bool = False,
     update_or_insert: bool = False,
 ) -> None:
+
+    logger.info(f"Starting dataset ingest")
     if json_path is None and json_data is None:
-        raise ValueError("Must provide either json_path or json_data")
+        logger.exception("Must provide either json_path or json_data")
     elif json_path is not None and json_data is not None:
-        raise ValueError("Must provide either json_path or json_data, not both")
+        logger.exception("Must provide either json_path or json_data, not both")
     elif json_path is not None:
+        logger.info(f"Reading json from {json_path}")
         path = Path(json_path)
         data = json.loads(path.read_text())
     else:
+        logger.info(f"Reading json from data")
         data = json_data
 
     DS = dutils.Dataset(**data)
     if update_or_insert:
         try:
+            logger.info(f"Trying to update dataset before inserting")
             dutils.update_dataset(DS)
         except Exception as e:
+            logger.info(f"Failed to update dataset, inserting instead")
             dutils.insert_dataset(DS)
     elif update:
+        logger.info(f"Updating dataset")
         dutils.update_dataset(DS)
     else:
+        logger.info(f"Inserting dataset")
         dutils.insert_dataset(DS)
+
+    logger.success(f"Finished dataset ingest")
--- src/gqcore/utils/ingest/ingest_feature_collection.py
+++ src/gqcore/utils/ingest/ingest_feature_collection.py
@@ -2,22 +2,33 @@
 import json
 from pathlib import Path
 
+from loguru import logger
+
 from gqcore.utils.db import features as futils
 
-
+@logger.catch(reraise=True)
 def ingest_feature_collection(json_path: str = None, json_data: dict = None, update: bool = False) -> None:
+
+    logger.info(f"Starting feature collection ingest")
+
     if json_path is None and json_data is None:
-        raise ValueError("Must provide either json_path or json_data")
+        logger.exception("Must provide either json_path or json_data")
     elif json_path is not None and json_data is not None:
-        raise ValueError("Must provide either json_path or json_data, not both")
+        logger.exception("Must provide either json_path or json_data, not both")
     elif json_path is not None:
+        logger.info(f"Reading json from {json_path}")
         path = Path(json_path)
         data = json.loads(path.read_text())
     else:
+        logger.info(f"Reading json from data")
         data = json_data
 
     FC = futils.FeatureCollection(**data)
     if update:
+        logger.info(f"Updating feature collection")
         futils.update_feature_collection(FC)
     else:
+        logger.info(f"Inserting feature collection")
         futils.insert_feature_collection(FC)
+
+    logger.success(f"Finished feature collection ingest")
