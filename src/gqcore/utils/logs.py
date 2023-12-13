af31701b4d13d6362881bbd9cc8e360262050dad
sgoodm <sgoodman@aiddata.wm.edu>
2023-12-13 12:43:24 -0500

    Update log format

--- src/gqcore/utils/logs.py
+++ src/gqcore/utils/logs.py
@@ -10,6 +10,7 @@ log_dir = Path(get_config()["logging"]["log_dir"])
 def get_logger(file_name: str) -> None:
     logger.add(
         (log_dir / file_name).with_suffix(".log"),
+        format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {name}:{file}:{function}:{line} | {message} | {extra}",
         rotation="1 week",
         level="DEBUG",
         backtrace=True,
