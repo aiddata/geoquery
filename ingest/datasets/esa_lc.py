6e50b152348d1cd73d5dff9a6f174ed4cccaf22f
sgoodm <sgoodman@aiddata.wm.edu>
2023-12-18 14:24:50 -0500

    Update ingests for k8s container paths

--- ingest/datasets/esa_lc.py
+++ ingest/datasets/esa_lc.py
@@ -3,4 +3,4 @@ from gqcore.utils.ingest.ingest_dataset import ingest_dataset
 
 get_logger("ingest")
 
-ingest_dataset(json_path="/home/userx/Desktop/geoquery-update/data/esa_lc/raster_ingest.json", update_or_insert=True)
+ingest_dataset(json_path="/tmp/geoquery-update/data/esa_lc/raster_ingest.json", update_or_insert=True)
--- ingest/datasets/ltdr_ndvi.py
+++ ingest/datasets/ltdr_ndvi.py
@@ -3,4 +3,4 @@ from gqcore.utils.ingest.ingest_dataset import ingest_dataset
 
 get_logger("ingest")
 
-ingest_dataset(json_path="/home/userx/Desktop/geoquery-update/data/avhrr_ndvi_v5/v5_yearly_raster_ingest.json", update_or_insert=True)
+ingest_dataset(json_path="/tmp/geoquery-update/data/avhrr_ndvi_v5/v5_yearly_raster_ingest.json", update_or_insert=True)
