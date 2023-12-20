2479cefb053152d2a6b00689a625168f20def7c6
sgoodm <sgoodman@aiddata.wm.edu>
2023-12-20 15:04:33 -0500

    Create general error handling task from existing clear dangling script, and build k8s cronjob for new task

--- src/gqcore/tasks/manage_errors.py
+++ src/gqcore/tasks/manage_errors.py
@@ -6,7 +6,11 @@ from gqcore.utils.logs import get_logger
 from gqcore.utils.db.extract_task_watch import free_dangling_tasks
 
 
-get_logger("clear_dangling_tasks")
+get_logger("errors")
+
+
+
+# ERROR TYPE - dangling extract tasks
 
 delay = timedelta(minutes=1)
 
@@ -15,3 +19,8 @@ logger.info(f"Clearing dangling tasks older than: {delay}")
 number_of_cleared_tasks = free_dangling_tasks(delay)
 
 logger.success(f"Cleared {number_of_cleared_tasks} dangling tasks.")
+
+
+# ERROR TYPE - TBD
+
+
