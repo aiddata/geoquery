56a3f6538c2fbd8dd29a52c9a72d1b37dae9e78b
sgoodm <sgoodman@aiddata.wm.edu>
2024-01-03 14:52:06 -0500

    Add static connection option

--- src/gqcore/utils/db/conn.py
+++ src/gqcore/utils/db/conn.py
@@ -22,3 +22,17 @@ def get_conn(**kwargs):
         with psycopg.connect(connect_str, **kwargs) as conn:
             logger.trace("Successfully connected to database")
             yield conn
+
+def get_static_conn(**kwargs):
+    if "row_factory" not in kwargs:
+        kwargs["row_factory"] = dict_row
+
+    # fetch config
+    config = get_config()
+
+    # make database connection and yield it
+    with logger.catch(exception=psycopg.Error, level="CRITICAL"):
+        connect_str = f'postgres://{config["postgis_username"]}:{config["postgis_password"]}@{config["postgis_address"]}:{config["postgis_port"]}/{config["postgis_dbname"]}'
+        conn = psycopg.connect(connect_str, **kwargs)
+        logger.trace("Successfully connected to database")
+        return conn
