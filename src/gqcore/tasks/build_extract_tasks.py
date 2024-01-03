16cbdee91d7460083745e9d0f676acdaf9e7117c
sgoodm <sgoodman@aiddata.wm.edu>
2024-01-03 14:52:53 -0500

    Add timing and logs to build extract tasks

--- src/gqcore/tasks/build_extract_tasks.py
+++ src/gqcore/tasks/build_extract_tasks.py
@@ -1,13 +1,24 @@
+import time
+
 from loguru import logger
 
 from gqcore.utils.logs import get_logger
 from gqcore.utils.db.extract_task_generation import generate_tasks
 
 
-get_logger("build_extract_tasks")
+if __name__ == "__main__":
 
-logger.info("Starting generating extract tasks")
+    get_logger("build_extract_tasks")
 
-generate_tasks(overwrite=True)
+    logger.info("Starting generating extract tasks")
 
-logger.success("Finished generating extract tasks")
+    t_start = time.perf_counter()
+    task_count = generate_tasks(overwrite=True)
+    t_end = time.perf_counter()
+
+    logger.success("Finished generating extract tasks")
+
+
+    logger.info(f"Time to generate tasks for {task_count} records: {t_end - t_start:0.4f} seconds")
+
+    logger.info(f"Avg time to generate tasks: {(t_end - t_start)/task_count:0.4f} seconds")
