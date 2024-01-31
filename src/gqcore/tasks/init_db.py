9038f938b8f16f67600d6a9f69c034c8bfe5a90e
Jacob Hall <email@jacobhall.net>
2024-01-31 12:05:42 -0500

    fix a couple of bugs in init_db task

--- src/gqcore/tasks/init_db.py
+++ src/gqcore/tasks/init_db.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 from gqcore.utils.db.init import init_pg_tables, init_pg_views
 from gqcore.utils.k8s.cluster import wait_for_db
 from gqcore.utils.logs import get_logger
@@ -11,9 +13,9 @@ if __name__ == "__main__":
     logger.info("initializing database...")
 
     # create tables
-    init_db(False)
+    init_pg_tables.init_db(False)
 
     # create views
-    init_views()
+    init_pg_views.init_views()
 
     logger.info("finished initializing database.")
