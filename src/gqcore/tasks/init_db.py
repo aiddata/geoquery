3bd1cd775bf6de9ed0fd661dc729f752f7596b93
Jacob Hall <email@jacobhall.net>
2024-02-23 16:57:39 -0500

    add docstrings to init_db task
    - black, isort

--- src/gqcore/tasks/init_db.py
+++ src/gqcore/tasks/init_db.py
@@ -1,10 +1,21 @@
+"""
+Module to orchestrate database initialization.
+"""
 from loguru import logger
 
 from gqcore.utils.db.init import init_pg_tables, init_pg_views
 from gqcore.utils.k8s.cluster import wait_for_db
 from gqcore.utils.logs import get_logger
 
-if __name__ == "__main__":
+
+def init_db() -> None:
+    """
+    Run database initialization utilities in order.
+
+    1. Wait for database to come online
+    2. Create tables
+    3. Create views
+    """
     get_logger("init_database")
 
     # wait for database to come online
@@ -19,3 +30,7 @@ if __name__ == "__main__":
     init_pg_views.init_views()
 
     logger.info("finished initializing database.")
+
+
+if __name__ == "__main__":
+    init_db()
