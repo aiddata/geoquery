75ab36288b9429eb44b973c82e27847665cb1e26
Jacob Hall <email@jacobhall.net>
2024-02-23 18:35:32 -0500

    add docstring to process_requests
    - move logic into a function
    - black, isort

--- src/gqcore/tasks/process_requests.py
+++ src/gqcore/tasks/process_requests.py
@@ -1,18 +1,30 @@
 """
-Code to process new and complete requests
-
+Module for processing new and complete requests
 """
 from loguru import logger
 
+from gqcore.utils.db.request_processing import (
+    process_completed_requests,
+    process_new_requests,
+)
 from gqcore.utils.logs import get_logger
-from gqcore.utils.db.request_processing import process_new_requests, process_completed_requests
 
-get_logger("process_requests")
 
-logger.info("Processing new requests")
-process_new_requests()
-logger.info("Finished processing new requests")
+def process_requests() -> None:
+    """
+    Process new and completed requests.
+    """
 
-logger.info("Processing completed requests")
-process_completed_requests()
-logger.info("Finished processing completed requests")
+    get_logger("process_requests")
+
+    logger.info("Processing new requests")
+    process_new_requests()
+    logger.info("Finished processing new requests")
+
+    logger.info("Processing completed requests")
+    process_completed_requests()
+    logger.info("Finished processing completed requests")
+
+
+if __name__ == "__main__":
+    process_requests()
