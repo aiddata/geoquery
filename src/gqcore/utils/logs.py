cf9d1c76d8164b34f00bf2fceb08d011a03106d5
Jacob Hall <email@jacobhall.net>
2024-02-23 19:04:03 -0500

    add docstrings to logs utility module
    - black, isort

--- src/gqcore/utils/logs.py
+++ src/gqcore/utils/logs.py
@@ -1,14 +1,19 @@
-from pathlib import Path
+"""Module to handle logs across entire GeoQuery backend."""
 import sys
+from pathlib import Path
 
 from loguru import logger
 
 from gqcore import get_config
 
-# log_dir = Path(get_config()["log_dir"])
-
 
 def get_logger(file_name: str) -> None:
+    """
+    Set up a loguru logging handler.
+
+    Parameters:
+      file_name: Name of log file to use (required, but not currently used)
+    """
     logger.add(
         sys.stderr,
         format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {name}:{file}:{function}:{line} | {message} | {extra}",
