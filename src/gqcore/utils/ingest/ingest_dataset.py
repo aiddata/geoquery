2230f0378dee26eee61c00eec0d7392641022db8
Jacob Hall <email@jacobhall.net>
2025-06-03 01:42:41 -0400

    tweaks to dataset ingest module

--- src/gqcore/utils/ingest/ingest_dataset.py
+++ src/gqcore/utils/ingest/ingest_dataset.py
@@ -10,8 +10,7 @@ from gqcore.utils.db import dataset as dutils
 
 @logger.catch(reraise=True)
 def ingest_dataset(
-    json_path: str = None,
-    json_data: dict = None,
+    json_data: dict | Path,
     update: bool = False,
     update_or_insert: bool = False,
 ) -> None:
@@ -20,37 +19,33 @@ def ingest_dataset(
     You must provide either the `json_path` or `json_data` parameter, but not both.
 
     Parameters:
-        json_path: Path to JSON metadata for the dataset.
-        json_data: JSON metadata for the dataset.
+        json_data: Path to JSON metadata, or a JSON dict, representing the dataset.
         update: Update the dataset (expecting it to already exist in the database).
         update_or_insert: Try to update the dataset, inserting it if it doesn't already exist.
     """
-    logger.info(f"Starting dataset ingest")
-    if json_path is None and json_data is None:
-        logger.exception("Must provide either json_path or json_data")
-    elif json_path is not None and json_data is not None:
-        logger.exception("Must provide either json_path or json_data, not both")
-    elif json_path is not None:
-        logger.info(f"Reading JSON from {json_path}")
-        path = Path(json_path)
-        data = json.loads(path.read_text())
-    else:
-        logger.info(f"Reading JSON from data")
+    if isinstance(json_data, Path):
+        logger.info(f"Reading JSON from {json_data}")
+        data = json.loads(json_data.read_text())
+    elif isinstance(json_data, dict):
+        logger.info("Reading JSON from data")
         data = json_data
+    else:
+        logger.exception("json must either be a dictionary or Path object")
 
-    DS = dutils.Dataset(**data)
+    dataset = dutils.Dataset(**data)
+
     if update_or_insert:
         try:
-            logger.info(f"Trying to update dataset before inserting")
-            dutils.update_dataset(DS)
-        except Exception as e:
-            logger.info(f"Failed to update dataset, inserting instead")
-            dutils.insert_dataset(DS)
+            logger.info("Trying to update dataset before inserting")
+            dutils.update_dataset(dataset)
+        except Exception:
+            logger.info("Failed to update dataset, inserting instead")
+            dutils.insert_dataset(dataset)
     elif update:
-        logger.info(f"Updating dataset")
-        dutils.update_dataset(DS)
+        logger.info("Updating dataset")
+        dutils.update_dataset(dataset)
     else:
-        logger.info(f"Inserting dataset")
-        dutils.insert_dataset(DS)
+        logger.info("Inserting dataset")
+        dutils.insert_dataset(dataset)
 
-    logger.success(f"Finished dataset ingest")
+    logger.success("Finished dataset ingest")
