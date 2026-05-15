27c8a7f927220d1d7c421f182197787eb5a58d07
sgoodm <sgoodman@aiddata.wm.edu>
2026-05-15 09:52:15 -0400

    Add coverage items to UI

--- backend/analytics/admin.py
+++ backend/analytics/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
+from django.db.models import Count
 
-# Register your models here.
+from .models import Coverage, ProcessingOption, ExtractTask, ExtractData, Request, RequestMap
+
+
+
+@admin.register(Coverage)
+class CoverageAdmin(admin.ModelAdmin):
+    list_display = ("dataset", "geom", "status")
+    list_filter = ("dataset", "geom", "status")
+    search_fields = ("dataset", "geom")
+    raw_id_fields = ("dataset", "geom")
