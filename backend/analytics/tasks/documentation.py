a9214172eeae0a3517f86c66a832786a90e5f218
sgoodm <sgoodman@aiddata.wm.edu>
2026-05-20 11:01:13 -0400

    Start to trim down and update doc building func (still work to do, maybe need to overhaul to markdown or something else)

---
+++ backend/analytics/tasks/documentation.py
@@ -0,0 +1,378 @@
+# accepts request object and creates pdf documentation
+
+import time
+import math
+from pathlib import Path
+
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak, Image, Table, TableStyle, KeepTogether
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib import colors
+from reportlab.lib.units import inch
+from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
+
+from datasets.models import Dataset, DatasetResource
+from features.models import Feature, FeatMap, FeatureCollection
+from analytics.models import ExtractTask, ProcessingOption, Request, RequestMap
+
+# =============================================================================
+
+styles = getSampleStyleSheet()
+
+def pg(text, pg_type):
+    """return paragraph of specified type for given text
+    """
+    if pg_type == 1:
+        return Paragraph(text, styles['Normal'])
+    elif pg_type == 2:
+        return Paragraph(text, styles['BodyText'])
+    else:
+        raise Exception("invalid paragraph type")
+
+
+def enforce_max_word_length(string, max_chars=80):
+    raw_word_list = string.split(" ")
+    short_word_list = []
+    for word in raw_word_list:
+        if len(word) > max_chars:
+            split_word = [
+                word[i:i+max_chars]
+                for i in range(0, len(word), max_chars)
+            ]
+            fixed_word = "\n".join(split_word)
+        else:
+            fixed_word = word
+        short_word_list += [fixed_word]
+    return " ".join(short_word_list)
+
+
+class DocBuilder():
+
+    def __init__(self, request, output_path, download_server, asset_dir):
+
+        self.request_id = str(request['id'])
+        self.request = request
+        self.output_path = output_path
+
+        self.assets_dir = Path(asset_dir)
+
+        self.download_server = download_server
+
+        self.doc = 0
+
+        # container for the 'Flowable' objects
+        self.Story = []
+
+        self.styles = getSampleStyleSheet()
+        self.styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+        self.styles.add(ParagraphStyle(name='Center', alignment=TA_CENTER))
+
+
+    def time_str(self, timestamp=None):
+        if timestamp != None:
+            try:
+                timestamp = int(timestamp)
+                if timestamp == 0:
+                    return "---"
+            except:
+                return "---"
+
+        return time.strftime('%Y-%m-%d %H:%M:%S (%Z)', time.localtime(timestamp))
+
+
+    def build_doc(self):
+        rid = self.request_id
+        print('build_doc: ' + rid)
+
+        self.doc = SimpleDocTemplate(self.output, pagesize=letter)
+
+        # build doc call all functions
+        self.add_header()
+        self.Story.append(Spacer(1, 0.5*inch))
+        self.add_info()
+        self.Story.append(Spacer(1, 0.3*inch))
+        self.add_timeline()
+        self.Story.append(Spacer(1, 0.3*inch))
+        self.add_cite_and_contents()
+        self.Story.append(PageBreak())
+
+        self.add_meta()
+        self.Story.append(PageBreak())
+
+        self.add_notes()
+        self.Story.append(PageBreak())
+
+        self.add_additional()
+
+        self.output_doc()
+
+        return True
+
+
+    # documentation header
+    def add_header(self):
+        # # aiddata logo
+        # logo = self.assets_dir + '/templates/aid_data.png'
+
+        # im = Image(logo, 2.188*inch, 0.5*inch)
+        # im.hAlign = 'LEFT'
+        # self.Story.append(im)
+
+        # self.Story.append(Spacer(1, 0.25*inch))
+
+        # title
+        ptext = '<font size=20>AidData GeoQuery Request Documentation</font>'
+        self.Story.append(Paragraph(ptext, self.styles['Center']))
+
+
+    # report generation info
+    def add_info(self):
+        ptext = '<b><font size=14>Report Info</font></b>'
+        self.Story.append(Paragraph(ptext, self.styles['BodyText']))
+        self.Story.append(Spacer(1, 0.1*inch))
+
+        data = [
+            ['Request Name', self.request['custom_name'].encode('utf8', 'replace')],
+            ['Request Id', str(self.request['id'])],
+            ['Email', self.request['contact']],
+            ['Generated on', self.time_str()],
+            ['Download Link', '<a href="http://{0}/query/#!/status/{1}">{0}/query/#!/status/{1}</a>'.format(
+                self.download_server, self.request['id'])]
+        ]
+
+        data = [[i[0], pg(i[1], 1)] for i in data]
+        t = Table(data)
+
+        t.setStyle(TableStyle([
+            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+            ('BOX', (0,0), (-1,-1), 0.25, colors.black)
+        ]))
+
+        self.Story.append(t)
+
+
+    # full request timeline / other processing info
+    def add_timeline(self):
+
+        ptext = '<b><font size=14>Processing Timeline</font></b>'
+        self.Story.append(Paragraph(ptext, self.styles['Normal']))
+        self.Story.append(Spacer(1, 0.1*inch))
+
+        data = [
+            ["submit_time", self.time_str(self.request['submit_time'])],
+            ["prepare_time", self.time_str(self.request['prepare_time'])],
+            ["processing_time", self.time_str(self.request['processing_time'])],
+            ["complete_time", self.time_str(int(time.time()))]
+        ]
+
+        data = [[i[0], pg(i[1], 1)] for i in data]
+        t = Table(data)
+
+        t.setStyle(TableStyle([
+            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+            ('BOX', (0,0), (-1,-1), 0.25, colors.black)
+        ]))
+
+        self.Story.append(t)
+
+
+    def add_cite_and_contents(self):
+
+        with open(self.assets_dir + '/templates/general.txt') as general:
+            for line in general:
+                p = Paragraph(line, self.styles['BodyText'])
+                self.Story.append(p)
+
+
+    # intro paragraphs
+    def add_notes(self):
+
+        with open(self.assets_dir + '/templates/field_names.txt') as field_names:
+            for line in field_names:
+                p = Paragraph(line, self.styles['BodyText'])
+                self.Story.append(p)
+
+        self.Story.append(PageBreak())
+
+        with open(self.assets_dir + '/templates/notes.txt') as field_names:
+            for line in field_names:
+                p = Paragraph(line, self.styles['BodyText'])
+                self.Story.append(p)
+
+        self.Story.append(PageBreak())
+
+        with open(self.assets_dir + '/templates/aid_data.txt') as field_names:
+            for line in field_names:
+                p = Paragraph(line, self.styles['BodyText'])
+                self.Story.append(p)
+
+
+    def add_meta(self):
+
+        ptext = '<b><font size=14>Meta Information</font></b>'
+        self.Story.append(Paragraph(ptext, self.styles['Normal']))
+        self.Story.append(Spacer(1, 0.25*inch))
+
+        # full boundary meta
+        ptext = '<font size=10><b>Boundary</b></font>'
+        self.Story.append(Paragraph(ptext, self.styles['Normal']))
+        self.Story.append(Spacer(1, 0.05*inch))
+
+
+        # build boundary meta table array
+        data = self.build_meta(self.request['boundary']['name'], 'boundary', None)
+
+        data = [[i[0], pg(i[1], 2)] for i in data]
+        t = Table(data)
+        t.setStyle(TableStyle([
+            ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+            ('BOX', (0,0), (-1,-1), 0.25, colors.black)
+        ]))
+
+        self.Story.append(t)
+        self.Story.append(Spacer(1, 0.1*inch))
+
+        meta_log = []
+
+        for dset in self.request['raster_data']:
+
+            if not dset:
+                continue
+
+            # if dset['name'] not in meta_log:
+            meta_log.append(dset['name'])
+
+            ptext = '<font size=10><b>Selection {0} - {1}</b></font>'.format(
+                len(meta_log), dset['custom_name'].encode('utf8', 'replace'))
+            self.Story.append(Paragraph(ptext, self.styles['Normal']))
+            self.Story.append(Spacer(1, 0.05*inch))
+
+            # build dataset meta table array
+            data = self.build_meta(dset['name'], dset['type'], dset)
+
+            data = [[i[0], pg(i[1], 2)] for i in data]
+            t = Table(data)
+            t.setStyle(TableStyle([
+                ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                ('BOX', (0,0), (-1,-1), 0.25, colors.black)
+            ]))
+
+            self.Story.append(KeepTogether(t))
+            self.Story.append(Spacer(1, 0.1*inch))
+
+
+    def build_meta(self, name, item_type, dset):
+
+        valid_item_types = ['boundary', 'dataset']
+        if item_type not in valid_item_types:
+            raise Exception(f'invalid item_type for build_meta ({item_type}), must be in: {valid_item_types}')
+
+        # get metadata for dataset or boundary
+        if item_type == 'boundary':
+            meta = FeatureCollection.objects.filter(name=name)
+        elif item_type == 'dataset':
+            meta = Dataset.objects.filter(name=name)
+
+        if meta is None:
+            msg = ('Could not lookup dataset ({0}, {1}) for '
+                   'build_meta').format(name, item_type)
+            raise Exception(msg)
+
+        details = "(no additional details)"
+        if "details" in meta:
+            details = meta["details"]
+
+        # build generic meta
+        data = [
+            ['Title', meta['title']],
+            ['Name', meta['name']],
+            ['Version', str(meta['version'])],
+        ]
+
+        if item_type == 'raster':
+
+            data.append(['Variable Description', meta['options']['variable_description']])
+            data.append(['Resolution', str(meta['options']['resolution'])])
+            # data.append(['Extract Types', ', '.join(meta['options']['extract_types'])])
+            data.append(['Factor', str(meta['options']['factor'])])
+
+            colnames_list =  [
+                '{0}.{1}.{2}'.format(dset['name'], i, j)
+                for i in [f['name'].split('_')[-1] for f in dset['files']]
+                for j in dset['options']['extract_types']
+            ]
+
+            colnames = ('Format: "{0}.&lt;temporal&gt;.&lt;method&gt;" <br /> '
+                        'for all combinations of &lt;temporal&gt; and &lt;method&gt; '
+                        'which can be found in the "Temporal Selection" and '
+                        '"Extract Types Selected" fields below '
+                        '({1} columns total)').format(
+                            dset['name'], len(colnames_list)
+                        )
+
+            data.append(['Column Names ', colnames])
+
+            # data.append(['Temporal Type', dset['temporal_type']])
+
+            temporal_raw = [f['name'].split('_')[-1] for f in dset['files']]
+
+            if 'none' in temporal_raw:
+                temporal_str = temporal_raw
+            else:
+                temporal_int = [int(s) for s in temporal_raw]
+                temporal_sorted = sorted(temporal_int, reverse=True)
+                temporal_str = [str(ts) for ts in temporal_sorted]
+
+            max_temporal_str_len = 25
+            for ix, i in enumerate(range(0, len(temporal_str), max_temporal_str_len)):
+                temporal_str_sub = temporal_str[i:i + max_temporal_str_len]
+                data.append(['Temporal Selection (' + str(ix) + ')', ', '.join(temporal_str_sub)])
+
+            # prevent issue due to missing extract_types_info field
+            for i in dset['options']['extract_types']:
+                if i not in meta['options']['extract_types_info']:
+                    meta['options']['extract_types_info'][i] = ""
+
+            data.append(['Extract Types Selected', ', '.join([
+                "{0} ({1})".format(i, meta['options']['extract_types_info'][i])
+                for i in dset['options']['extract_types']
+            ])])
+
+        data.append(['',''])
+        data.append(['Description', meta['description']])
+        data.append(['Details', details])
+
+        data.append(['Bounding Box', str(meta['spatial']['coordinates'])])
+
+        data.append(['Date Added', str(meta['asdf']['date_added'])])
+        data.append(['Date Updated', str(meta['asdf']['date_updated'])])
+
+        if 'sources_name' in meta['extras']:
+            data.append(['Source Name', meta['extras']['sources_name']])
+
+        if 'sources_web' in meta['extras']:
+            tmp_sources_web = meta['extras']['sources_web']
+            tmp_sources_web = enforce_max_word_length(tmp_sources_web)
+            data.append(['Source Link', tmp_sources_web])
+
+        if 'citation' in meta['extras']:
+            tmp_citation = meta['extras']['citation']
+            tmp_citation = enforce_max_word_length(tmp_citation)
+            data.append(['Citation', tmp_citation])
+
+        return data
+
+
+    # license stuff
+    def add_additional(self):
+
+        with open(self.assets_dir + '/templates/additional.txt') as license:
+            for line in license:
+                p = Paragraph(line, self.styles['BodyText'])
+                self.Story.append(p)
+
+
+    # write the document to disk
+    def output_doc(self):
+        self.doc.build(self.Story)
+        return "Success"
