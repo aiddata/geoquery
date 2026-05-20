4d1d2ccbed2571976e5687752a9801afa856a233
sgoodm <sgoodman@aiddata.wm.edu>
2026-05-20 17:00:14 -0400

    Add admin displays for additional models

--- backend/analytics/admin.py
+++ backend/analytics/admin.py
@@ -12,3 +12,37 @@ class CoverageAdmin(admin.ModelAdmin):
     list_filter = ("dataset", "geom", "status")
     search_fields = ("dataset", "geom")
     raw_id_fields = ("dataset", "geom")
+
+
+@admin.register(ProcessingOption)
+class ProcessingOptionAdmin(admin.ModelAdmin):
+    list_display = ("id", "short_name", "description")
+    search_fields = ("id", "short_name", "description")
+
+
+@admin.register(ExtractTask)
+class ExtractTaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "status", "submit_time", "start_time", "update_time", "complete_time")
+    list_filter = ("status", "submit_time", "start_time", "update_time", "complete_time")
+    search_fields = ("id",)
+
+
+@admin.register(ExtractData)
+class ExtractDataAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "data_column", "float_value", "int_value", "str_value")
+    list_filter = ("id", "name")
+    search_fields = ("id", "name")
+
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ("id", "contact", "status", "submit_time", "prepare_time", "process_time", "complete_time")
+    list_filter = ("status", "submit_time")
+    search_fields = ("id", "contact")
+
+
+# @admin.register(RequestMap)
+# class RequestMapAdmin(admin.ModelAdmin):
+#     list_display = ("req_id", "task_id")
+#     list_filter = ("req_id", "task_id")
+#     search_fields = ("req_id", "task_id")
