965f272be2270c6976501f15113ecf49fb9c271d
sgoodm <sgoodman@aiddata.wm.edu>
2026-06-01 14:26:05 -0400

    Add signal to refresh matviews on FeatureCollection create/update

--- backend/features/signals.py
+++ backend/features/signals.py
@@ -1,12 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Feature
+from .models import Feature, FeatureCollection
 
 
 @receiver(post_save, sender=Feature)
-def on_feature_created(sender, instance, created, **kwargs):
+def on_feature_created(_sender, instance, created, **_kwargs):
     if not created:
         return
     from analytics.tasks.coverage import create_coverage_records_for_feature
     create_coverage_records_for_feature(instance.id)
+
+
+@receiver(post_save, sender=FeatureCollection)
+def on_feature_collection_saved(_sender, _instance, **_kwargs):
+    from features.matviews import refresh_materialized_views
+    refresh_materialized_views()
