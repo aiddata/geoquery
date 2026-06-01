509986f19b76c6dabd02263af927785b73ad72a5
sgoodm <sgoodman@aiddata.wm.edu>
2026-06-01 14:41:24 -0400

    Use Celery chain to sequence request processing before task dispatch on submission

--- backend/analytics/signals.py
+++ backend/analytics/signals.py
@@ -1,3 +1,4 @@
+from celery import chain
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -9,5 +10,4 @@ def on_request_submitted(_sender, instance, created, **_kwargs):
     if not created:
         return
     from analytics.tasks.maintenance import process_user_requests, dispatch_processing_tasks
-    process_user_requests.delay()
-    dispatch_processing_tasks.delay()
+    chain(process_user_requests.s(), dispatch_processing_tasks.s()).delay()
