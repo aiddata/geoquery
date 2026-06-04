4685d158ad32e595011153bedbd433d2afa1449a
Jacob Hall <email@jacobhall.net>
2026-06-04 14:08:56 -0400

    another try at rabbitmq config

--- backend/geoquery/settings.py
+++ backend/geoquery/settings.py
@@ -169,7 +169,6 @@ CELERY_BROKER_URL = os.environ.get(
     "CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//"
 )
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "django-db")
-CELERY_RESULT_EXTENDED = True
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
