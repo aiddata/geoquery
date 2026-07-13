9ce8a1160a85feac756bd40d87bf485647416b2b
sgoodm <sgoodman@aiddata.wm.edu>
2026-07-13 14:38:39 -0400

    Force enable debug for testing

--- backend/geoquery/settings.py
+++ backend/geoquery/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in ("true", "1", "yes", "on")
+if not DEBUG:
+    DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
